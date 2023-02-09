efb8a0cde15752ad0936b01d8363767a38c2ac6a
Dr. Cat Lu <bfcat.cn@outlook.com>
2023-02-09 15:21:36 +0800

    Update audiodialog.py
    patch for windows 11

--- audiodialog.py
+++ audiodialog.py
@@ -32,8 +32,25 @@ class AudioDialog(QtWidgets.QDialog):
         self.deviceCombo.addItems(device_names)
         self.deviceCombo.blockSignals(False)
         default_device = QtMultimedia.QAudioDeviceInfo.defaultInputDevice()
-        self.default_device_index = audio_devices.index(default_device)
+        # Modified to running on windows 11
+        # self.default_device_index = audio_devices.index(default_device)
+        # self.deviceCombo.setCurrentIndex(self.default_device_index)
+        import platform
+        plat = platform.system().lower()
+        if plat == 'windows': # choose first device as default for windows
+            self.default_device_index = 0
+        else : # OK for linux
+            self.default_device_index = audio_devices.index(default_device)
+
         self.deviceCombo.setCurrentIndex(self.default_device_index)
+        # It seems lost default sample rate
+        # add the sample rate for default device
+        self.device_rates = self.audio_devices[self.device_index].supportedSampleRates()
+        self.sampleRateCombo.blockSignals(True)
+        self.sampleRateCombo.clear()
+        self.sampleRateCombo.addItems([str(x) for x in self.device_rates])
+        self.sampleRateCombo.blockSignals(False)
+        self.sampleRateCombo.setCurrentIndex(0) # choose Min
 
     def device_selected(self):
         self.device_index = self.deviceCombo.currentIndex()
